3cba11805c878a02158ef6744008ee2cfe8f1074
Fernando Via Canel <fernando.canel@klarna.com>
2016-11-11 11:08:00 +0100

    Add uniqueName decorator

---
+++ src/uniqueName.js
@@ -0,0 +1,31 @@
+import React, {PureComponent} from 'react'
+import seededRandom from 'seed-random'
+
+const TOKENS = /[xy]/g
+
+const uuid = (seed) => {
+  const random = seed === undefined ? Math.random : seededRandom(seed)
+
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(TOKENS, (c) => {
+    const r = random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+
+let counter = 0
+export default (Target) => {
+  class UniqueName extends PureComponent {
+    constructor () {
+      super()
+
+      this.uniqueName = uniqueName(
+        `${Target.displayName || Target.name}-${uuid(counter++)}`
+      )
+    }
+
+    render () {
+      return <Target name={this.uniqueName} {...this.props} />
+    }
+  }
+}
