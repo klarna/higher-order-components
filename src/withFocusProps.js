d2b8999e717e12d2fc126577094a3a0ac6cd4988
Fernando Via Canel <fernando.canel@klarna.com>
2017-02-15 13:39:18 +0100

    Add withFocusProps higher-order component

---
+++ src/withFocusProps.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+
+export default focusProps => Target => class WithFocusProps extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      focused: false
+    }
+  }
+
+  render () {
+    const {onFocus, onBlur, ...props} = this.props
+    const {focused} = this.state
+
+    return <Target
+      onFocus={(...args) => {
+        this.setState({focused: true})
+        onFocus && onFocus(...args)
+      }}
+      onBlur={(...args) => {
+        this.setState({focused: false})
+        onBlur && onBlur(...args)
+      }}
+      {...props}
+      {...focused ? focusProps : {}}
+    />
+  }
+}
