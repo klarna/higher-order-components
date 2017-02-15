bc7ffa055dcde1cac5a589a31f7ff53dd542c3fb
Fernando Via Canel <fernando.canel@klarna.com>
2017-02-15 13:32:15 +0100

    Add withTouchProps higher-order component

---
+++ src/withTouchProps.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+
+export default touchProps => Target => class WithTouchProps extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      touched: false
+    }
+  }
+
+  render () {
+    const {onTouchStart, onTouchEnd, ...props} = this.props
+    const {touched} = this.state
+
+    return <Target
+      onTouchStart={(...args) => {
+        this.setState({touched: true})
+        onTouchStart && onTouchStart(...args)
+      }}
+      onTouchEnd={(...args) => {
+        this.setState({touched: false})
+        onTouchEnd && onTouchEnd(...args)
+      }}
+      {...props}
+      {...touched ? touchProps : {}}
+    />
+  }
+}
