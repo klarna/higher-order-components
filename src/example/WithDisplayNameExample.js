a54f338458b8f4b8cc34c783f27861174f46fa94
Fernando Via Canel <fernando.canel@klarna.com>
2017-06-05 17:03:48 +0200

    Add live withDisplayName example

---
+++ src/example/WithDisplayNameExample.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { withDisplayName } from '../'
+
+export default withDisplayName('Has')('Long')('Namespaced')('Name')(function ComponentToBeWrappedWithADifferentName() {
+  return (
+    <article>
+      <h1>withDisplayName</h1>
+      <p>This one will have a long custom name when inspecting it with the React DevTools</p>
+    </article>
+  )
+})
--- src/example/index.js
+++ src/example/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import WithDeprecationWarningExample from './WithDeprecationWarningExample'
+import WithDisplayNameExample from './WithDisplayNameExample'
 
 const Example = ({ children }) => (
   <div
@@ -38,7 +39,7 @@ render(
         <WithDeprecationWarningExample />
       </Example>
       <Example>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <WithDisplayNameExample />
       </Example>
     </section>
   </main>,
