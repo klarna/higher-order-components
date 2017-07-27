25921bf80f49ab3bb5c74a8ea74c3785b2110aae
Fernando Via Canel <fernando.canel@klarna.com>
2017-07-27 16:26:34 +0200

    Add withStyleSheetOverride to Sagui config

--- sagui.config.js
+++ sagui.config.js
@@ -12,6 +12,7 @@ module.exports = {
     'withNotifyOnLowFPS',
     'withHoverProps',
     'withMouseDownProps',
+    'withStyleSheetOverride',
     'withTheme',
     'withTouchProps',
     'withUncontrolledProp',
