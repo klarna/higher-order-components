0b987a05788ca16c1165870f2bf52eac766e20f0
moriaam <moria.am@gmail.com>
2017-08-01 10:00:15 +0300

    Add autofill hoc

---
+++ src/withAutofillProps.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import wrapDisplayName from 'recompose/wrapDisplayName'
+
+const startAnimationName = 'onAutofillStart'
+const endAnimationName = 'onAutofillCancel'
+
+const isWebkit = () => navigator.userAgent.indexOf('WebKit') !== -1
+
+const emptyAnimation = animationName => {
+  return `
+    @keyframes ${animationName} {
+      from {/**/}
+      to {/**/}
+    }
+  `
+}
+
+const injectStyle = style => {
+  const styleElement = document.createElement('style')
+
+  document.head.appendChild(styleElement)
+
+  let styleSheet = styleElement.sheet
+
+  styleSheet.insertRule(style, styleSheet.cssRules.length)
+}
+
+function injectKeyFrames() {
+  const autofillStartEmptyKeyframesStyle = emptyAnimation(startAnimationName)
+  const autofillEndEmptyKeyframesStyle = emptyAnimation(endAnimationName)
+
+  injectStyle(autofillStartEmptyKeyframesStyle)
+  injectStyle(autofillEndEmptyKeyframesStyle)
+}
+
+function injectAutofillHook() {
+  const autofillHook = `
+    input:-webkit-autofill {
+      // Expose a hook for JavaScript when autofill is shown.
+      // JavaScript can capture 'animationstart' events
+      animation-name: ${startAnimationName};
+
+      // Make the backgound color become yellow _really slowly_
+      transition: background-color 50000s ease-in-out 0s;
+    }
+  `
+
+  const notAutofillHook = `
+    input:not(:-webkit-autofill) {
+      // Expose a hook for JavaScript when autofill is no longer shown.
+      // JavaScript can capture 'animationstart' events
+      animation-name: ${endAnimationName};
+    }`
+
+  injectStyle(autofillHook)
+  injectStyle(notAutofillHook)
+}
+
+const registerAutofill = () => {
+  const autofillInjected =
+    window.__klarna_ui_components && window.__klarna_ui_components.isAutofillInjected
+
+  if (!autofillInjected && isWebkit()) {
+    injectKeyFrames()
+    injectAutofillHook()
+
+    if (window.__klarna_ui_components) {
+      window.__klarna_ui_components.isAutofillInjected = true
+    } else {
+      window.__klarna_ui_components = { isAutofillInjected: true }
+    }
+  }
+}
+
+export default autofillProps => Target => {
+  class WithAutofillProps extends Component {
+    constructor() {
+      super()
+
+      registerAutofill()
+
+      this.state = { autofill: false }
+    }
+
+    handleAnimation(animationName) {
+      switch (animationName) {
+        case startAnimationName:
+          return this.setState({ autofill: true })
+
+        case endAnimationName:
+          return this.setState({ autofill: false })
+      }
+    }
+
+    render() {
+      const { autofill } = this.state
+
+      return (
+        <Target
+          onAnimationStart={this.handleAnimation.bind(this)}
+          {...this.props}
+          {...autofill && autofillProps}
+        />
+      )
+    }
+  }
+
+  WithAutofillProps.displayName = wrapDisplayName(Target, 'withAutofillProps')
+
+  return WithAutofillProps
+}
