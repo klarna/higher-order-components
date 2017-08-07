7801eed9fffa15171c732b32f0aae6d16af67e72
moriaam <moria.am@gmail.com>
2017-08-07 11:50:51 +0300

    Add tests

--- src/withAutofillProps.spec.js
+++ src/withAutofillProps.spec.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { render } from 'react-dom'
 import withAutofillProps from './withAutofillProps'
 import { equal } from 'assert'
 
@@ -11,5 +12,52 @@ describe('withAutofillProps', () => {
     const EnhancedInput = withAutofillProps({ autofill: true })(Input)
 
     equal(EnhancedInput.displayName, 'withAutofillProps(Input)')
+  })
+
+  it('has the autofilled prop when autofill animation gets triggered', () => {
+    const root = document.createElement('div')
+    let animationStartCallback
+
+    function Target({ autofilled, onAnimationStart }) {
+      animationStartCallback = onAnimationStart
+
+      return <div>{autofilled ? 'autofilled' : 'not autofilled'}</div>
+    }
+
+    const DecoratedTarget = withAutofillProps({
+      autofilled: true,
+    })(Target)
+
+    render(<DecoratedTarget />, root)
+
+    equal(root.innerText, 'not autofilled')
+
+    animationStartCallback('onAutofillStart')
+
+    equal(root.innerText, 'autofilled')
+  })
+
+  it('does not have the autofilled prop when autofill animation gets canceled', () => {
+    const root = document.createElement('div')
+    let animationStartCallback
+
+    function Target({ autofilled, onAnimationStart }) {
+      animationStartCallback = onAnimationStart
+
+      return <div>{autofilled ? 'autofilled' : 'not autofilled'}</div>
+    }
+
+    const DecoratedTarget = withAutofillProps({
+      autofilled: true,
+    })(Target)
+
+    render(<DecoratedTarget />, root)
+
+    animationStartCallback('onAutofillStart')
+    equal(root.innerText, 'autofilled')
+
+    animationStartCallback('onAutofillCancel')
+
+    equal(root.innerText, 'not autofilled')
   })
 })
